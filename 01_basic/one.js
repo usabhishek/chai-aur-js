e420e4173d5473403b07f6bc91a603b60c7e07ad
usabhishek <inabhishek14@gmail.com>
2025-07-26 18:30:21 +0000

    day 1

---
+++ 01_basic/one.js
@@ -0,0 +1,18 @@
+const id = 12345;
+var name = "Abhishek"
+let age = 18;
+nationality = "Indian"; // It is also allowed but try to minimize its use
+let date; //It gives o/p undefined
+
+
+//id = 55 // Modification in const keyword is not allowed
+console.log(id); // Thorws TypeErorr
+
+name = "Hitesh"
+age = 30;
+nationality = "American";
+
+console.table([id,name,age,nationality,date]);
+
+//Always prefer const and let keyword due to it resolves the issue of block scope and functional scope
+//In javascript semicolon is not necessary because JS follow ASI(Automatic semicolon insertion) but keep in practise to avoid ASI pitfalls
---
+++ 01_basic/two.js
@@ -0,0 +1,12 @@
+// String -- prefer "" due to better readability
+// Number
+// Boolean-- Represents true or false
+// Null -- A special value that means "no value" / standalone value
+// Undefined -- A variable that has been declared but not assigned a value
+// Symbol -- A unique and immutable identifier
+// BigInt -- represent large integer
+
+"use strict"; // treat all JS code as newer version
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object 
