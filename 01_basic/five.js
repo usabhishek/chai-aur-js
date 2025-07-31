f1c3e9994292cce3227fc20afe021d2222ec812b
usabhishek <inabhishek14@gmail.com>
2025-07-31 13:48:34 +0000

    added datatype summary , stack and heap memory files

---
+++ 01_basic/five.js
@@ -0,0 +1,56 @@
+// Datatype summary
+
+//  Primitive
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const Id = Symbol('124');
+const anotherId = Symbol('124')
+
+// console.log(Id == anotherId) -return false
+// console.log(Id == anotherId)  -returns false
+
+// ****************************************************************************************
+
+//Non primitive(Reference type)
+// Array , objects , functions
+
+const array1 = ["Hello","Hi",123];
+
+let myobj = {
+    name : "Abhishek",
+    age : 22
+}
+//dtype -object  -->console.log(typeof myobj);
+
+const myFunc = function(){
+    console.log("Hello world");
+}
+myFunc(); //output - Hello world
+//dtype -object function
+
+// **********************************************************************************************
+
+// Dynamic typing means you don’t need to declare the type of a variable when you create it. The type is determined at runtime, based on the value assigned.
+// let x = 42;      ->x is a number
+// x = "hello";     ->now x is a string
+
+// *****************************************************************************************
+//stack memory (primitive) and heap memory(non-primitive)
+let myname = "Abhishek";
+let anothername = myname;
+anothername = "Hitesh";
+
+console.log(myname);
+console.log(anothername);
+
+
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user@ybl"
+}
+let userTwo = userOne;
+
+userTwo.email="Abhi@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
