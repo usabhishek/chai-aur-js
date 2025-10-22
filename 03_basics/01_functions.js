96d5ae624ee31e892d43d793bfc6f5843e9cbaed
Abhishek kashyap <inabhishek14@gmail.com>
2025-10-22 19:17:33 +0000

    functions part1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,23 @@
+function sayMyName(){
+    console.log("A");
+    console.log("B");
+    console.log("H");
+    console.log("I");
+    console.log("S");
+    console.log("H");
+    console.log("E");
+    console.log("K");
+}
+sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2; return result;
+    return number1 + number2;
+}
+
+const result = addTwoNumbers(3, 5);
+console.log("Result: ",result);
